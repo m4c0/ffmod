2b20ef2eca5c65c850e3399172961cb168f02131
Eduardo Costa <m4c0@users.noreply.github.com>
2024-02-19 13:04:13 +0000

    poc: merging update thread into main
    one thread to rule them all... let real usage define how to split the
    work

--- poc.cpp
+++ poc.cpp
@@ -6,24 +6,6 @@ import voo;
 
 static constexpr const auto filename = "movie.mov";
 
-class player : public voo::update_thread {
-  voo::h2l_image m_frame;
-
-  void build_cmd_buf(vee::command_buffer cb) override {
-    voo::cmd_buf_one_time_submit pcb{cb};
-    m_frame.setup_copy(*pcb);
-  }
-
-public:
-  explicit player(voo::device_and_queue *dq, unsigned w, unsigned h)
-      : update_thread{dq}
-      , m_frame{*dq, w, h} {}
-
-  [[nodiscard]] constexpr auto iv() const noexcept { return m_frame.iv(); }
-
-  using update_thread::run;
-};
-
 class thread : public voo::casein_thread {
 public:
   void run() override {
@@ -35,11 +17,15 @@ public:
     auto vctx = ffmod::avcodec_open_best(fmt_ctx, AVMEDIA_TYPE_VIDEO);
     auto actx = ffmod::avcodec_open_best(fmt_ctx, AVMEDIA_TYPE_AUDIO);
 
+    voo::h2l_image frm;
+
     while (!interrupted()) {
       voo::swapchain_and_stuff sw{dq};
 
       extent_loop(dq, sw, [&] {
         sw.queue_one_time_submit(dq, [&](auto pcb) {
+          frm.setup_copy(*pcb);
+
           auto scb = sw.cmd_render_pass(pcb);
           ;
         });
